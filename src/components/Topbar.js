1b9dfad9063fff59580a9d1c50e2b77e674a2730
Isabela Duarte <isabeladuartelbg@gmail.com>
2022-10-23 02:42:58 -0300

    Remove focus

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -25,7 +25,7 @@ export default function Topbar() {
               <Link to="/home" className="text-xl font-semibold mx-5 my-3 text-black hover:text-black" style={{ textDecoration: 'none' }}>AupaMatch</Link>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
-            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
               <span className="sr-only">Open menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </Popover.Button>
@@ -78,7 +78,7 @@ export default function Topbar() {
                   </p>
                 </div>
                 <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                     <span className="sr-only">Close menu</span>
                     <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
